c5ce2edb771b7110d4344850dcf6263a2f73a5be
Aizan Magomadova <maai1807@akershus-fk.no>
2019-09-04 16:39:58 +0200

    ...

--- Uke 36/sirkel/sketch.js
+++ Uke 36/sirkel/sketch.js
@@ -1,7 +1,6 @@
 function setup() {
   // put setup code here
   createCanvas(400, 400);
-
 }
 var x = 0; var y = 0;
 var fartX = 3;var fartY=2;
@@ -14,8 +13,11 @@ function draw() {
   if (x > width){
      fartX =-3;fill("gray");
   } else if (x<0) {
-    fartX= 3;fill("pink")
-  } else if (y>height){fartY =-2;fill("white")}
-  else if (y<0){fartY =2;fill("green")}
+    fartX= 3;
+    fill("pink")
+  } else if (y>height){fartY =-2;
+    fill("white")}
+    else if (y<0){fartY =2;
+    fill("green")}
   x=x+fartX;y=y+fartY;
 }
